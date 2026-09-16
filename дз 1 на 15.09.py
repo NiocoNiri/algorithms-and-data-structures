8f2a70da2fbeeb94b1c43a9d4327ef042132ddb5
Egor <84263950+NiocoNiri@users.noreply.github.com>
2026-09-16 11:00:29 +0300

    Update дз 1 на 15.09.py

--- дз 1 на 15.09.py
+++ дз 1 на 15.09.py
@@ -4,12 +4,16 @@ from numpy.typing import NDArray
 
 class deque:
     def __init__(self, arraySize: int) -> None:
-        self.myArray: NDArray[np.int32] = np.empty(arraySize, dtype=np.int32)
+        self.myArray: NDArray[np.int32] = np.empty(
+            arraySize,
+            dtype=np.int32
+        )
 
         self.capacity = arraySize
         self.size = 0
         self.head = 0
         self.tail = 0
+
 
     def accessByIndex(self, index: np.int32) -> np.int32:
         if index < 0 or index >= self.size:
@@ -18,6 +22,7 @@ class deque:
         real_index = (self.head + index) % self.capacity
 
         return self.myArray[real_index]
+
 
     def addToTail(self, num: np.int32) -> np.int32:
         if self.size == self.capacity:
@@ -30,6 +35,7 @@ class deque:
 
         return num
 
+
     def addToHead(self, num: np.int32) -> np.int32:
         if self.size == self.capacity:
             raise OverflowError("Deque заполнена")
@@ -37,20 +43,33 @@ class deque:
         self.head = (self.head - 1) % self.capacity
 
         self.myArray[self.head] = num
-
         self.size += 1
 
         return num
 
-d = deque(5)
 
-d.addToTail(np.int32(1))
-d.addToTail(np.int32(2))
-d.addToTail(np.int32(3))
+    def removeFromHead(self) -> np.int32:
+        if self.size == 0:
+            raise IndexError("Deque пустая")
 
-d.addToHead(np.int32(4))
 
-print(d.accessByIndex(np.int32(0)))  
-print(d.accessByIndex(np.int32(1)))  
-print(d.accessByIndex(np.int32(2)))  
-print(d.accessByIndex(np.int32(3)))  
+        value = self.myArray[self.head]
+
+
+        self.head = (self.head + 1) % self.capacity
+
+        self.size -= 1
+
+        return value
+
+
+    def removeFromTail(self) -> np.int32:
+        if self.size == 0:
+            raise IndexError("Deque пустая")
+        self.tail = (self.tail - 1) % self.capacity
+
+        value = self.myArray[self.tail]
+
+        self.size -= 1
+
+        return value
